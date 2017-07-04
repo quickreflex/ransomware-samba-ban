8d6cf00a9c03d0d80069e9936b0e2ae4087daa28
quickreflex <kptodorov@abv.bg>
2017-07-04 17:51:46 +0300

    Add files via upload

--- ransomware-samba-ban.sh
+++ ransomware-samba-ban.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
 
-function string_replace {
-    #DOC: "${string/match/replace}"
-    string=$1
-    echo "${string//$2/$3}"
-}
-
 curl -o /tmp/fsrm.json https://fsrm.experiant.ca/api/v1/combined
 jq -r .filters[] /tmp/fsrm.json > /tmp/fsrm.lst
 
-i=0
-mapfile -t known_ransom < /tmp/fsrm.lst
+sed -i 's/\./\\./g; s/\[/\\[/g; s/\]/\\]/g; s/(/\\(/g; s/)/\\)/g; s/{/\\{/g; s/}/\\}/g; s/\@/\\@/g; s/\$/\\$/g' /tmp/fsrm.lst
+sed -i 's/\ /\\ /g; s/\!/\\!/g; s/\#/\\#/g; s/\+/\\+/g; s/\-/\\-/g; s/\;/\\;/g; s/\,/\\,/g; s/\~/\\~/g; s/'\''/\\'\''/g' /tmp/fsrm.lst
 
-for ln in "${known_ransom[@]}"; do
-    ln=$(string_replace "$ln" "\." "\.")
-    ln=$(string_replace "$ln" "\[" "\[")
-    ln=$(string_replace "$ln" "\]" "\]")
-    ln=$(string_replace "$ln" "\(" "\(")
-    ln=$(string_replace "$ln" "\)" "\)")
-    ln=$(string_replace "$ln" "\{" "\{")
-    ln=$(string_replace "$ln" "\}" "\}")
-    ln=$(string_replace "$ln" "\@" "\@")
-    ln=$(string_replace "$ln" "\$" "\\$")
-    ln=$(string_replace "$ln" "\ " "\ ")
-    ln=$(string_replace "$ln" "\!" "\!")
-    ln=$(string_replace "$ln" "\#" "\#")
-    ln=$(string_replace "$ln" "\+" "\+")
-    ln=$(string_replace "$ln" "\-" "\-")
-    ln=$(string_replace "$ln" "\;" "\;")
-    ln=$(string_replace "$ln" "\," "\,")
-    ln=$(string_replace "$ln" "\'" "\'")
-    ln=$(string_replace "$ln" "\~" "\~")
-    known_ransom[$i]="$ln"
-    i=$((i+1))
-done
+mapfile -t known_ransom < /tmp/fsrm.lst
 
 defInt=$(route | grep '^default' | grep -o '[^ ]*$')
 
