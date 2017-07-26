6197dd3ae72df9a42d8585c0ed10fabb48c08cd1
quickreflex <kptodorov@abv.bg>
2017-07-26 11:55:53 +0300

    Add files via upload
    Change the algorithm to increase the performance of the script.

--- ransomware-samba-ban.sh
+++ ransomware-samba-ban.sh
@@ -8,28 +8,27 @@ done
 
 jq -r .filters[] /tmp/fsrm.json > /tmp/fsrm.lst
 
-sed -i 's/\./\\./g; s/\[/\\[/g; s/\]/\\]/g; s/(/\\(/g; s/)/\\)/g; s/{/\\{/g; s/}/\\}/g; s/\@/\\@/g; s/\$/\\$/g' /tmp/fsrm.lst
-sed -i 's/\ /\\ /g; s/\!/\\!/g; s/\#/\\#/g; s/\+/\\+/g; s/\-/\\-/g; s/\;/\\;/g; s/\,/\\,/g; s/\~/\\~/g; s/'\''/\\'\''/g' /tmp/fsrm.lst
+sed -i 's/\./\\./g; s/\[/\\[/g; s/\]/\\]/g;  s/\@/\\@/g; s/\$/\\$/g; s/\ /\\ /g; s/\!/\\!/g' /tmp/fsrm.lst
+sed -i 's/\#/\\#/g; s/\+/\\+/g; s/\-/\\-/g; s/\;/\\;/g; s/\,/\\,/g; s/\~/\\~/g; s/'\''/\\'\''/g; s/\*/\.*/g' /tmp/fsrm.lst
 
 mapfile -t known_ransom < /tmp/fsrm.lst
 
 defInt=$(route | grep '^default' | grep -o '[^ ]*$')
 
 while inotifywait -e modify /var/log/messages; do
-    grep '"*smbd"*' /var/log/messages > /tmp/samba_log.lst
+    : > /tmp/samba_log.lst
+    for ln in "${known_ransom[@]}"; do
+          grep --text ".*smbd.*$ln.$" /var/log/messages >> /tmp/samba_log.lst
+    done
     while read line; do
-          for ln in "${known_ransom[@]}"; do
-               if [[ "$line" = *smbd*$ln ]]; then
-                   OIFS="$IFS"
-                   IFS='[=,|]'
-                   read -a clientIP <<< "${line}"
-                   IFS="$OIFS"
-                   chkRule=$(iptables '-L' '-n' | grep '^DROP.*${clientIP[1]//[[:space:]]/}.*')
-                   if [[ $chkRule == "" ]]; then
-                        iptables -I INPUT -i $defInt -s ${clientIP[1]//[[:space:]]/} -j DROP
-                        echo "Detected ransomware activity from this ip: "${clientIP[1]//[[:space:]]/} >> /var/log/ransomware_ban.log
-                   fi
-               fi
-          done
+          OIFS="$IFS"
+          IFS='[=,|]'
+          read -a clientIP <<< "${line}"
+          IFS="$OIFS"
+          chkRule=$(iptables '-L' '-n' | grep '^DROP.*${clientIP[1]//[[:space:]]/}.*')
+          if [[ $chkRule == "" ]]; then
+                  iptables -I INPUT -i $defInt -s ${clientIP[1]//[[:space:]]/} -j DROP
+                  echo "Detected ransomware activity from this ip: "${clientIP[1]//[[:space:]]/} >> /var/log/ransomware_ban.log
+          fi
     done < /tmp/samba_log.lst
 done
